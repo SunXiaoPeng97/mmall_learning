06366940dc84c937d949143fd0b4b266b2869f59
SunXiaoPeng <975257566@qq.com>
2019-07-09 04:17:41 +0800

    购物车模块通过测试。

---
+++ src/main/java/com/mmal/service/IProductService.java
@@ -0,0 +1,23 @@
+package com.mmal.service;
+
+import com.github.pagehelper.PageInfo;
+import com.mmal.common.ServerResponse;
+import com.mmal.pojo.Product;
+import com.mmal.vo.ProductDetailVo;
+
+public interface IProductService {
+
+    ServerResponse saveOrUpdateProduct(Product product);
+
+    ServerResponse<String> setSaleStatus(Integer productId, Integer status);
+
+    ServerResponse<ProductDetailVo> manageProductDetail(Integer productId);
+
+    ServerResponse<PageInfo> getProductList(int pageNum, int pageSize);
+
+    ServerResponse<PageInfo> searchProduct(String productName, Integer productId, int pageNum, int pageSize);
+
+    ServerResponse<ProductDetailVo> getProductDetail(Integer productId);
+
+    ServerResponse<PageInfo> getProductByKeywordCategory(String keyword, Integer categoryId, int pageNum, int pageSize, String orderBy);
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,18 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+
+
+springmvc上传文件
+<form name="form1" action="http://localhost:8088/mmal_war/manage/product/upload.do" method="post" enctype="multipart/form-data">
+    <input type="file" name="upload_file" multiple="multiple"/>
+    <input type="submit" value="springmvc上传文件"/>
+</form>
+
+富文本图片上传
+<form name="form2" action="http://localhost:8088/mmal_war/manage/product/richtext_img_upload.do" method="post" enctype="multipart/form-data">
+    <input type="file" name="upload_file"/>
+    <input type="submit" value="富文本图片上传文件"/>
+</form>
 </body>
 </html>
